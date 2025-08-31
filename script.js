97ea13010ea06c837db984955c3aacc3a64ac706
Sagar Srivastava <sagarsrivastava5201@gmail.com>
2025-08-31 20:45:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const progressBar = document.getElementById('progress-bar');
+  if(progressBar){
+    setTimeout(() => {
+      progressBar.style.width = '60%';
+    }, 500);
+  }
+  
+  
+});
+
+
